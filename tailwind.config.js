679c138df9012b56edb997450bb4df816e95bf87
Andrei Fokau <andrei@funnel.io>
2021-08-22 16:17:27 +0200

    jekyll-postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
         // './_posts/*.md',
         '*.html',
     ],
-    mode: 'jit',
+    // mode: 'jit', // doesn't work with jekyll-postcss plugin https://github.com/tailwindlabs/tailwindcss-jit/issues/41
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
